e109d4d1d3403c6c253da30561f15f7724c6d250
estilbee <odaouphars@gmail.com>
2022-09-25 15:14:43 -0400

    editing javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,27 @@
 // Assignment Code
+
+function generatePassword(){
+
+  var passwordLength = parseInt(prompt("How long would you like your password to be?"));
+  
+  var charSet = ["0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&**()ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+  for (var i = 0; i < passwordLength ; i ++) {
+      var randomNumber = Math.floor (Math.random() * charSet.length);
+  }
+
+  return password;
+  
+}
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
